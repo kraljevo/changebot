7f35ab25794dae416480dfd1212a5c6cbde4593d
Matthew Tetrault <mktetrault@yahoo.com>
2019-05-30 19:14:23 -0400

    Update changebot.js

--- changebot.js
+++ changebot.js
@@ -1,1 +1,10 @@
-//Test change
+/*In this repo, write a bot using the GitHub api
+that automatically accepts any pull requests to
+this repo after X amount of time if the pull request
+receives more thumbs up emoji reacts than thumbs
+down. Then whenever a new pull request is accepted
+to the master branch, the bot should pull the latest
+version of the master branch and restart itself
+using that code.
+*/
+
